44a55b221a8e06d6b6ff7cd4bb50a2d3dd32bcbe
jhugosp <juan.huguet@wizeline.com>
2024-10-09 14:51:15 -0500

    ADD: tailwind import and styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
+import './index.css';
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="dark">
+      <body  className="bg-white dark:bg-slate-700 text-white min-h-full">
         {children}
       </body>
     </html>
