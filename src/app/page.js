eae66d3a5dc4cbfc017929bc74903d10c8072d38
jhugosp <juan.huguet@wizeline.com>
2024-10-12 20:30:56 -0500

    ADD: container styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,13 @@ import HomePage from "./home/page";
 
 export default function Home() {
   return (
-    <div className="mt-64">
-      <p className="text-4xl font-medium text-center mb-24">Biblioteca académica - GaUNAL</p>
-      <HomePage />
-      <Disclaimer />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="container bg-[#71717a] shadow-lg rounded-lg py-8 md:p-16 mx-auto">
+        <p className="text-4xl font-medium text-center mb-16">Biblioteca académica - GaUNAL</p>
+        <HomePage />
+        <Disclaimer />
+      </div>
     </div>
+
   );
 }
