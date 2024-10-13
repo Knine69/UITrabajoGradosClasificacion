1014c8e8d7243cf5041f045e03557c1a482b1619
jhugosp <juan.huguet@wizeline.com>
2024-10-12 20:31:03 -0500

    ADD: image transparent bg

--- src/app/disclaimer/paje.js
+++ src/app/disclaimer/paje.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Disclaimer() {
     return (
-        <div className="ml-80 w-3/5">
+        <div className="ml-24 w-4/5">
             <div className="flex justify-between">
-                <Image src="/grupo_gaunal.jpeg" width={120} height={100} className="ml-10 my-6"/>
-                <Image src="/unal.png" width={150} height={100} className="ml-10 my-6"/>
-                <Image src="/poli.png" width={250} height={100} className="ml-10 my-6"/>
+                <Image src="/output-onlinepngtools.png" width={250} height={80} className="ml-10 my-6"/>
+                <Image src="/unal.png" width={300} height={80} className="ml-10 my-6"/>
+                <Image src="/poli.png" width={400} height={100} className="ml-10 my-6"/>
             </div>
         </div>
     );
