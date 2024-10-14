2771c8e1b8281e4b67ebc7d584dca1cfcc06eb62
jhugosp <juan.huguet@wizeline.com>
2024-10-14 12:25:26 -0500

    ADD: chat behavior with backend

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,72 @@
+"use client";
 import FilesUploader from "./uploader/page.js"
 import DynamicParagraphs from "./paragraphs/page.js";
+import { useState } from "react";
 
 export default function HomePage() {
+
+  const [paragraphs, setParagraphs] = useState([
+    { text: 'Initial paragraph from user', emitter: 'user' },
+    { text: 'Initial paragraph from checker', emitter: 'checker' },
+]);
+
+async function handleFormSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget)
+  const formValues = Object.fromEntries(formData.entries());
+  try {
+
+    setParagraphs(prevParagraphs => [...prevParagraphs, {"text": formValues.search, "emitter": "user"}]);
+    const response = await fetch('http://localhost:5000/chroma/documents', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "collection_name": "some_collection",
+        "category": "quimica",
+        "user_query": formValues.search
+      })
+    });
+
+    formValues.search = ''
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    
+    let { done, value } = await reader.read();
+    const newParagraphs = []
+    while (!done) {
+      const chunk = decoder.decode(value, { stream: true });
+      const events = chunk.split("\n\n"); // Split on SSE event boundary
+      
+      console.log(chunk)
+
+      events.forEach(event => {
+        if (event.startsWith("data:")) {
+          const jsonData = event.replace("data: ", "");
+          const parsedData = JSON.parse(jsonData);
+          // console.log("Received data:", parsedData);
+          newParagraphs.push({"text": parsedData.DESCRIPTION, "emitter": "checker"});
+        }
+      });
+      
+      ({ done, value } = await reader.read());
+    }
+
+    console.log("Stream finished");
+    setParagraphs(prevParagraphs => [...prevParagraphs, ...newParagraphs]);
+  } catch (error) {
+    console.error('Error during fetch or streaming:', error);
+  }
+}
+
+
   return (
     <div className="flex flex-col items-center justify-center">
       <FilesUploader />
       {/* Searchbar component */}
-      <form className="w-full flex justify-center">
+      <form className="w-full flex justify-center" onSubmit={handleFormSubmit}>
         <div className="flex w-3/6">
           <label className="relative block text-[#6366f1] w-full">
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
@@ -31,7 +91,7 @@ export default function HomePage() {
         </div>
       </form>
 
-      <DynamicParagraphs />
+      <DynamicParagraphs paragraphs={paragraphs}/>
     </div>
   );
 }
--- src/app/home/paragraphs/page.js
+++ src/app/home/paragraphs/page.js
@@ -1,28 +1,10 @@
 "use client"
 import { useState } from 'react';
 
-export default function DynamicParagraphs() {
-    const [paragraphs, setParagraphs] = useState([
-        { text: 'Initial paragraph from user', emitter: 'user' },
-        { text: 'Initial paragraph from checker', emitter: 'checker' },
-    ]);
-
-    const addUserParagraph = () => {
-        setParagraphs([
-            ...paragraphs,
-            { text: 'New generated paragraph from user', emitter: 'user' },
-        ]);
-    };
-
-    const addCheckerParagraph = () => {
-        setParagraphs([
-            ...paragraphs,
-            { text: 'New generated paragraph from checker', emitter: 'checker' },
-        ]);
-    };
+export default function DynamicParagraphs({ paragraphs }) {
 
     return (
-        <div className="flex flex-col mb-10 mt-5 dark:bg-slate-800 w-5/6 rounded p-8">
+        <div className="flex flex-col mb-0.5 mt-5 dark:bg-slate-600 w-5/6 rounded-md p-8">
             <div className=' h-96 overflow-y-scroll'>
                 {paragraphs.map((paragraph, index) => (
                     <div
@@ -30,30 +12,15 @@ export default function DynamicParagraphs() {
                         className={`flex mb-5 w-full ${paragraph.emitter === 'user' ? 'justify-end' : 'justify-start'}`}
                     >
                         <div
-                            className={`text-l font-medium focus:border-blue-900 focus:ring-blue-900 w-auto overflow-scroll overscroll-auto p-3 border border-slate-50 rounded-md ${paragraph.emitter === 'user'
-                                ? 'text-white bg-blue-500'
-                                : 'text-black bg-green-300'
+                            className={`text-l font-medium focus:border-blue-900 focus:ring-blue-900 w-auto overflow-scroll overscroll-auto p-3 border border-slate-50 rounded text-white ${paragraph.emitter === 'user'
+                                ? 'bg-[#1e1b4b]'
+                                : 'bg-[#4a044e]'
                                 }`}
                         >
                             {paragraph.text}
                         </div>
                     </div>
                 ))}
-            </div>
-
-            <div className="mt-4 ml-32">
-                <button
-                    onClick={addUserParagraph}
-                    className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-                >
-                    Add User Paragraph
-                </button>
-                <button
-                    onClick={addCheckerParagraph}
-                    className="bg-green-500 text-white px-4 py-2 rounded"
-                >
-                    Add Checker Paragraph
-                </button>
             </div>
         </div>
 
