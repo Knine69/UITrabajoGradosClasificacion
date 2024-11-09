ced5ed2d31d3d1493f4352716887ccd740d9a825
jhugosp <juan.huguet@wizeline.com>
2024-11-09 10:56:08 -0500

    ADD: response simulation backend

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -60,6 +60,13 @@ export default function HomePage() {
       setParagraphs(prevParagraphs => [...prevParagraphs, ...newParagraphs]);
     } catch (error) {
       console.error('Error during fetch or streaming:', error);
+
+      // Simulation response
+
+      let response = prompt("Response received from Backend: ")
+      if (response.length > 0) {
+        setParagraphs(previous => [...previous, { text: response, emitter: constantVariables.CHECKER_EMITTER }])
+      }
     }
   }
 
