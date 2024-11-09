1737cef97b8c67c0cc1cb6c4cbd7b27de68609a8
jhugosp <juan.huguet@wizeline.com>
2024-11-09 10:55:54 -0500

    REFACTOR: logos used

--- src/app/disclaimer/paje.js
+++ src/app/disclaimer/paje.js
@@ -4,9 +4,9 @@ export default function Disclaimer() {
     return (
         <div className="ml-24 w-4/5">
             <div className="flex justify-between">
-                <Image src="/output-onlinepngtools.png" width={250} height={80} className="ml-10 my-6"/>
+                <Image src="/grupo_gaunal.png" width={350} height={80} className="ml-10 my-6"/>
                 <Image src="/unal.png" width={300} height={80} className="ml-10 my-6"/>
-                <Image src="/poli.png" width={400} height={100} className="ml-10 my-6"/>
+                <Image src="/politecnico.png" width={400} height={100} className="ml-10 my-6"/>
             </div>
         </div>
     );
