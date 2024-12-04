04e53dc3748d005c6cbb65e96a04909b0ec8d7aa
knine69 <jhuguetospina@gmail.com>
2024-12-04 17:20:31 -0500

    REFACTOR: changed endpoint for chroma domain

--- src/app/home/constants/constants.js
+++ src/app/home/constants/constants.js
@@ -1,7 +1,7 @@
 export const constantVariables = {
     COLLECTION_NAME: "gaunal_collection",
     CHECKER_EMITTER: "emitter",
-    CHROMA_DOMAIN: "http://localhost:5000/chroma/",
+    CHROMA_DOMAIN: "http://192.168.0.71:5000/chroma/",
     CHROMA_UPLOAD_FILE_ENDPOINT: "form_embed_document",
     CHROMA_MAKE_QUERY_ENDPOINT: "documents"
 }
