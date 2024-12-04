94511bbf03f6b62167624a11874ff9211d1f884a
knine69 <jhuguetospina@gmail.com>
2024-12-04 17:47:44 -0500

    ADD: tailwind config plugin

--- src/app/home/paragraphs/page.js
+++ src/app/home/paragraphs/page.js
@@ -1,27 +1,24 @@
-"use client"
+"use client";
 
 export default function DynamicParagraphs({ paragraphs = [] }) {
-
-    return (
-        <div className="flex flex-col mb-0.5 mt-5 dark:bg-slate-600 shadow-2xl w-5/6 rounded-md p-8">
-            <div className=' h-96 overflow-y-scroll'>
-                {paragraphs.map((paragraph, index) => (
-                    <div
-                        key={index}
-                        className={`flex mb-5 w-full ${paragraph.emitter === 'user' ? 'justify-end' : 'justify-start'}`}
-                    >
-                        <div
-                            className={`text-l font-medium focus:border-blue-900 focus:ring-blue-900 w-auto overflow-scroll overscroll-auto p-3 rounded text-white ${paragraph.emitter === 'user'
-                                ? 'bg-[#1e1b4b]'
-                                : 'bg-[#4a044e]'
-                                }`}
-                        >
-                            {paragraph.text}
-                        </div>
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="h-96 overflow-y-scroll scrollbar-hide">
+      {paragraphs.map((paragraph, index) => (
+        <div
+          key={index}
+          className={`flex mb-5 w-full ${
+            paragraph.emitter === "user" ? "justify-end" : "justify-start"
+          }`}
+        >
+          <div
+            className={`text-l font-medium focus:border-blue-900 focus:ring-blue-900 w-auto overflow-scroll overscroll-auto p-3 rounded text-white ${
+              paragraph.emitter === "user" ? "bg-[#1e1b4b]" : "bg-[#4a044e]"
+            }`}
+          >
+            {paragraph.text}
+          </div>
         </div>
-
-    );
+      ))}
+    </div>
+  );
 }
