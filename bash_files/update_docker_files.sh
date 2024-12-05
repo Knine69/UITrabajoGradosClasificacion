a521f8d71d5f43ae57c6b1400948cd0dd78f3b04
knine69 <jhuguetospina@gmail.com>
2024-12-05 12:34:43 -0500

    UPDATE: img version

--- bash_files/update_docker_files.sh
+++ bash_files/update_docker_files.sh
@@ -2,7 +2,7 @@
 
 echo "Building and updating Gaunal Library UI Docker Image..."
 
-docker build -f Dockerfile --platform linux/amd64 -t jhuguet/gaunal-library-ui:4.0.5 .
-docker tag gaunal-library-ui:4.0.5 jhuguet/gaunal-library-ui:latest
-docker push jhuguet/gaunal-library-ui:4.0.5
+docker build -f Dockerfile --platform linux/amd64 -t jhuguet/gaunal-library-ui:4.0.6 .
+docker tag gaunal-library-ui:4.0.6 jhuguet/gaunal-library-ui:latest
+docker push jhuguet/gaunal-library-ui:4.0.6
 
