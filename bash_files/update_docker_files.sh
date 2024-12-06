1831c0dfe43bfaef8efe924b1da4e43bd1962e6e
knine69 <jhuguetospina@gmail.com>
2024-12-06 11:18:50 -0500

    REFACTOR: accessing result object

--- bash_files/update_docker_files.sh
+++ bash_files/update_docker_files.sh
@@ -2,7 +2,7 @@
 
 echo "Building and updating Gaunal Library UI Docker Image..."
 
-docker build -f Dockerfile --platform linux/amd64 -t jhuguet/gaunal-library-ui:4.0.11 .
-docker tag gaunal-library-ui:4.0.11 jhuguet/gaunal-library-ui:latest
-docker push jhuguet/gaunal-library-ui:4.0.11
+docker build -f Dockerfile --platform linux/amd64 -t jhuguet/gaunal-library-ui:4.0.12 .
+docker tag gaunal-library-ui:4.0.12 jhuguet/gaunal-library-ui:latest
+docker push jhuguet/gaunal-library-ui:4.0.12
 
